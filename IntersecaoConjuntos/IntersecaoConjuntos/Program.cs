e4c30eee07c06aa3af7db94c8b7f5a9e1318cd22
Jackson Moraes <jacksontadeu@hotmail.com>
2024-10-29 13:13:44 -0300

    :sparkles: feat: Exercicios sobre uniao e intersecao de conjuntos

---
+++ IntersecaoConjuntos/IntersecaoConjuntos/Program.cs
@@ -0,0 +1,42 @@
+﻿//Algoritmo para fazer a interseção de dois conjuntos
+HashSet<int> A = new HashSet<int>(); // Criando os conjuntos
+HashSet<int> B = new HashSet<int>();
+string resp;
+//Pedindo os elementos para adiconar ao conjunto A
+Console.WriteLine("Criando o primeiro conjunto");
+do
+{
+    
+    Console.Write("Digite um numero: ");
+    int num = int.Parse(Console.ReadLine());
+    A.Add(num);
+    Console.Write("Deseja adicionar mais(s/n): ");
+    resp = Console.ReadLine();
+    
+  
+}while (resp != "n") ;
+//Pedindo os elementos para adiconar ao conjunto B
+Console.WriteLine("Criando o segundo conjunto");
+do
+{
+    
+    Console.Write("Digite um numero: ");
+    int num = int.Parse(Console.ReadLine());
+    B.Add(num);
+    Console.Write("Deseja adicionar mais(s/n): ");
+    resp = Console.ReadLine();
+    
+  
+}while (resp != "n") ;
+// Realizando a interseção entre os dois conjuntos
+A.IntersectWith(B);
+
+//Imprimindo resultado da interseção
+Console.WriteLine("Interseção dos conjuntos");
+Console.Write("Conjunto após a interseção: ");
+foreach (var numero in A)
+{
+    Console.Write(numero + " ");
+}
+
+Console.ReadKey();
---
+++ UniaoConjuntos/UniaoConjuntos/Program.cs
@@ -0,0 +1,43 @@
+﻿
+//Algoritmo para fazer a união de dois conjuntos
+HashSet<int> A = new HashSet<int>(); // Criando os conjuntos
+HashSet<int> B = new HashSet<int>();
+string resp;
+//Pedindo os elementos para adiconar ao conjunto A
+Console.WriteLine("Criando o primeiro conjunto");
+do
+{
+    
+    Console.Write("Digite um numero: ");
+    int num = int.Parse(Console.ReadLine());
+    A.Add(num);
+    Console.Write("Deseja adicionar mais(s/n): ");
+    resp = Console.ReadLine();
+    
+  
+}while (resp != "n") ;
+//Pedindo os elementos para adiconar ao conjunto B
+Console.WriteLine("Criando o segundo conjunto");
+do
+{
+    
+    Console.Write("Digite um numero: ");
+    int num = int.Parse(Console.ReadLine());
+    B.Add(num);
+    Console.Write("Deseja adicionar mais(s/n): ");
+    resp = Console.ReadLine();
+    
+  
+}while (resp != "n") ;
+// Realizando a união entre os dois conjuntos
+A.UnionWith(B);
+
+//Imprimindo resultado da união
+Console.WriteLine("União dos conjuntos");
+Console.Write("Conjunto após a uniao: ");
+foreach (var numero in A)
+{
+    Console.Write(numero + " ");
+}
+
+Console.ReadKey();
